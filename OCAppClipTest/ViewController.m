44e18ce027e1fd29f435951c29c232cb5c9c93e1
陈黔 <chenq1@bsoft.com.cn>
2020-10-13 16:57:47 +0800

    Initial commit

---
+++ OCAppClipTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  OCAppClipTest
+//
+//  Created by BlackChen on 2020/10/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ OCClipTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  OCClipTest
+//
+//  Created by BlackChen on 2020/10/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
